06a4170de4a6e7c039341fb6af41ae627c976631
minsu.lee <minsu.rob@gmail.com>
2023-11-17 17:16:33 +0900

    feat: 깃추가

---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyANd4yZT54bSK0AcbItj1bqB7q5x78fOT0",
+  authDomain: "nwitter-reloaded.firebaseapp.com",
+  projectId: "nwitter-reloaded",
+  storageBucket: "nwitter-reloaded.appspot.com",
+  messagingSenderId: "597055701730",
+  appId: "1:597055701730:web:ad1f4784fbb491e2a614bb",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
